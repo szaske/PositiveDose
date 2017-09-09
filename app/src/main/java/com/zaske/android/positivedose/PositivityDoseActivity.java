e1848ddf4c9e62580b03f39e7ab9e2fdd89eda26
szaske <github@zaske.com>
2017-09-09 11:58:22 -0700

    Initial commit

---
+++ app/src/main/java/com/zaske/android/positivedose/PositivityDoseActivity.java
@@ -0,0 +1,13 @@
+package com.zaske.android.positivedose;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PositivityDoseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_positivity_dose);
+    }
+}
